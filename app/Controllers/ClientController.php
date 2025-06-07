1790ee9974511029a7bb457f7ba7983efd44994d
VictorNAGomes <dev.victorgomes@gmail.com>
2025-06-07 01:05:26 -0300

    fix autoload

---
+++ app/Controllers/ClientController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers;
+
+class ClientController 
+{
+    public function index()
+    {
+        echo "Hello World!";
+    }
+}
--- app/index.php
+++ app/index.php
@@ -1,5 +1,9 @@
-<?php
+<?php 
 
-echo "Hello, World!";
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ ."/routes.php";
 
-?>
+use App\Core\Core as Core;
+use App\Http\Route;
+
+Core::dispatch(Route::routes());
